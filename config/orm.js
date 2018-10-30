d298846f0221d859342b2f3717a9dca5f97be6ed
Don <lordashym@gmail.com>
2018-10-29 19:57:08 -0500

    it works

---
+++ config/orm.js
@@ -0,0 +1,84 @@
+var connection = require ('./connection.js');
+
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+
+	return arr.toString();
+}
+
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		arr.push(key + "=" + ob[key]);
+	}
+
+	return arr.toString();
+}
+
+var orm = {
+	selectAll: function(tableInput, cb) {
+		var queryString = "SELECT * FROM " + tableInput + ";";
+
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+
+	insertOne: function(table, cols, vals, cb) {
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
+
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	},
+
+	// Function that updates a single table entry
+	updateOne: function(table, objColVals, condition, cb) {
+		// Construct the query string that updates a single entry in the target table
+		var queryString = "UPDATE " + table;
+
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	}
+};
+
+// Export the orm object for use in other modules
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured)VALUES("Bacaon Cheese Burger", FALSE);
+INSERT INTO burgers(burger_name, devoured)VALUES("Triple Deluxe Burger", FALSE);
+INSERT INTO burgers(burger_name, devoured)VALUES("Cheese Burger", FALSE);
